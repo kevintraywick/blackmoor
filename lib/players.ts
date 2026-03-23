251d7da7a5e0beaf0a787eb5afac8781d83b981d
kevintraywick <49107699+kevintraywick@users.noreply.github.com>
2026-03-23 09:54:45 -0500

    Switch Nicole's portrait to Nicole-HollyGo2.png

--- lib/players.ts
+++ lib/players.ts
@@ -2,7 +2,7 @@
 export const PLAYERS = [
   { id: 'levi',     playerName: 'LEVI',     character: 'Garrick',  initial: 'L', img: '/images/players/levi.png' },
   { id: 'jeanette', playerName: 'JEANETTE', character: 'Eleil',    initial: 'J', img: '/images/players/jeanette.png' },
-  { id: 'nicole',   playerName: 'NICOLE',   character: 'HollyGo',  initial: 'N', img: '/images/players/nicole.png' },
+  { id: 'nicole',   playerName: 'NICOLE',   character: 'HollyGo',  initial: 'N', img: '/images/players/Nicole-HollyGo2.png' },
   { id: 'katie',    playerName: 'KATIE',    character: 'Lysandra', initial: 'K', img: '/images/players/katie.png' },
   { id: 'brandon',  playerName: 'BRANDON',  character: 'Vaoker',   initial: 'B', img: '/images/players/brandon.png' },
   { id: 'ashton',   playerName: 'ASHTON',   character: 'Ash',      initial: 'A', img: '/images/players/ashton.png' },
